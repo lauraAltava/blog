88f3f4244643261192c0437d0c2e869da0398e8b
lauraAltava <laumaltava@gmail.com>
2023-10-31 13:02:00 +0100

    falta ultima parte

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -15,4 +15,31 @@ class PageController extends AbstractController
             'controller_name' => 'PageController',
         ]);
     }
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {   
+        return $this->render('page/about.html.twig', []);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {   
+        return $this->render('page/contact.html.twig', []);
+    }
+
+    #[Route('/blog', name: 'blog')]
+    public function blog(): Response
+    {   
+        return $this->render('page/blog.html.twig', []);
+    }
+    #[Route('/single_post', name: 'single_post')]
+    public function single_post(): Response
+    {   
+        return $this->render('page/single_post.html.twig', []);
+    }
+
+
+
+
+
 }
